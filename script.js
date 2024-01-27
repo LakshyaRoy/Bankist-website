a4e7c4e30dd3e6c31280d4bf586e419bb8033eba
lakshya <lakshyaroy848@gmail.com>
2024-01-27 23:27:07 +0530

    js pratice .

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnCloseModal.forEach(btn => btn.addEventListener('click', openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 // for (let i = 0; i < btnsOpenModal.length; i++)
 //   btnsOpenModal[i].addEventListener('click', openModal);
@@ -31,4 +31,57 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+// const header = document.querySelector('.header');
+// // creating and inserting elemnets
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent = 'We use Cookies for improved functionality and analytics';
+// message.innerHTML =
+//   'We use Cookies for improved functionality and analytics. <button class="btn btn--close-cookie"> Got it </button>';
+
+// // header.prepend(message);
+// header.append(message);
+
+// // header.before(message);
+// // header.after(message);
+
+// // delete elements
+
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
+
+// // styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '98.9vw';
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+// console.log(getComputedStyle(message).height);
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function () {
+  // const s1coods = section1.getBoundingClientRect();
+  // console.log(s1coods);
+
+  // window.scrollTo(s1coods.left + window.scrollX, s1coods.top + window.scrollY);
+
+  // console.log(s1coods.left + window.scrollX);
+  // console.log(s1coods.top + window.scrollY);
+
+  // window.scrollTo({
+  //   left: s1coods.left + window.scrollX,
+  //   top: s1coods.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
 });
